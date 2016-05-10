8f79fd4b04656fd870bcc72db3b18fc2f14861b7
oscar <oscar@oscar.(none)>
2016-05-10 09:32:11 +0200

    final version

--- process_frame.c
+++ process_frame.c
@@ -13,12 +13,16 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 #define IMG_SIZE NUM_COLORS*OSC_CAM_MAX_IMAGE_WIDTH*OSC_CAM_MAX_IMAGE_HEIGHT
 
 const int nc = OSC_CAM_MAX_IMAGE_WIDTH;
 const int nr = OSC_CAM_MAX_IMAGE_HEIGHT;
 
 int TextColor;
+int16 imgDx[IMG_SIZE];
+int16 imgDy[IMG_SIZE];
+
 
 float bgrImg[IMG_SIZE];
 
@@ -36,7 +40,7 @@ const int MinArea = 500;
 struct OSC_VIS_REGIONS ImgRegions;/* these contain the foreground objects */
 
 void ChangeDetection();
-void SetBackground();
+//void SetBackground();
 void Erode_3x3(int InIndex, int OutIndex);
 void Dilate_3x3(int InIndex, int OutIndex);
 void DetectRegions();
@@ -44,7 +48,7 @@ void DrawBoundingBoxes();
 
 void ResetProcess()
 {
-	SetBackground();
+	//SetBackground();
 }
 
 
@@ -52,7 +56,7 @@ void ProcessFrame()
 {
 	//initialize counters
 	if(data.ipc.state.nStepCounter == 1) {
-		SetBackground();
+		//SetBackground();
 
 	} else {
 
@@ -64,61 +68,101 @@ void ProcessFrame()
 		DetectRegions();
 
 		DrawBoundingBoxes();
+
+		calcAngle();
 	}
 }
 
 void ChangeDetection() {
-	int r, c;
-	//set result buffer to zero
-	memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+int r, c;
+//set result buffer to zero
+memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+//loop over the rows
+for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+//loop over the columns
+	for(c = Border; c < (nc-Border); c++) {
+		unsigned char* p = &data.u8TempImage[SENSORIMG][r+c];
 
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			float pImg = data.u8TempImage[SENSORIMG][r+c];
-			float pBgr = bgrImg[r+c];
-			float Dif = fabs(pImg-pBgr);
 
-			//if the difference is larger than threshold value (can be changed on web interface)
-			if(Dif > data.ipc.state.nThreshold) {
-				//set pixel value to 255 in BACKGROUND image (only the blue plane)
-				data.u8TempImage[THRESHOLD][r+c] = 255;
-				//increase foreground counter
-				data.u8TempImage[INDEX1][r+c]++;
-				//check whether limit is reached
-				if(data.u8TempImage[INDEX1][r+c] == frgLimit) {
-					//set pixel to background
-					bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-					data.u8TempImage[INDEX1][r+c] = 0;
+/* implement Sobel filter in x-direction */
+		int32 dx =
+			-(int32) *(p-nc-1) + (int32) *(p-nc+1)
+			-2* (int32) *(p-1) + 2* (int32) *(p+1)
+			-(int32) *(p+nc-1) + (int32) *(p+nc+1);
+
+		int32 dy = -(int32) *(p-nc-1) -2* (int32) *(p-nc) - (int32) *(p-nc+1)
+					+(int32) *(p+nc-1) +2* (int32) *(p+nc) + (int32) *(p+nc+1);
+
+
+/* check if norm is larger than threshold */
+		int32 df2 = dx*dx+dy*dy;
+		int32 thr2 = data.ipc.state.nThreshold*data.ipc.state.nThreshold;
+		if(df2 > thr2) {//avoid square root
+	//set pixel value to 255 in THRESHOLD image for gui
+			data.u8TempImage[THRESHOLD][r+c] = 255;
+		}
+	//store derivatives (int16 is enough)
+		imgDx[r+c] = (int16) dx;
+		imgDy[r+c] = (int16) dy;
+	//possibility to visualize data
+		data.u8TempImage[BACKGROUND][r+c] = (uint8) MAX(0, MIN(255,128+dx));
+		}
+	}
+}
+
+void calcAngle(){
+	int o,c = 0;
+	//loop over objects
+	for(o = 0; o < ImgRegions.noOfObjects; o++) {
+//get pointer to root run of current object
+		if(ImgRegions.objects[o].area > MinArea) {
+			int w =0;
+			int x = 0;
+			int y = 0;
+			int z = 0;
+			struct OSC_VIS_REGIONS_RUN* currentRun = ImgRegions.objects[o].root;
+//loop over runs of current object
+			char* str="   ";
+
+			do {
+//loop over pixel of current run
+
+				for(c = currentRun->startColumn; c <= currentRun->endColumn; c++) {
+					int r = currentRun->row;
+//processing for individual pixel at row r and column c
+					double angle = atan2(imgDy[r*nc+c], imgDx[r*nc+c]);
+
+					if(angle < 0){
+						angle= angle+3.1415;
+					}
+					if(angle != 0.001){
+						if(((0 < angle) && (angle <= 0.3926)) || ((2.7488 < angle) && (angle <= 3.1415))){
+							w = w+1;
+						}else if((0.3926 < angle) && (angle <= 1.1781)){
+							x = x+1;
+						}else if((1.1781 < angle) && (angle <= 1.9635)){
+							y = y+1;
+						}else{
+							z = z+1;
+						}
+					}
+					if((w>x)&&(w>y)&&(w>z)){
+						str=" 0";
+					}else if((x>w)&&(x>y)&&(x>z)){
+						str=" 45";
+					}else if((y>w)&&(y>x)&&(y>z)){
+						str=" 90";
+					}else{
+						str="135";
+					}
 				}
-			} else {
-				// update background image
-				bgrImg[r+c] = avgFac*bgrImg[r+c] + (1-avgFac)*(float) data.u8TempImage[SENSORIMG][r+c];
-				// set value for display
-				data.u8TempImage[BACKGROUND][r+c] = (unsigned char) bgrImg[r+c];
-				//set foreground counter to zero
-				data.u8TempImage[INDEX1][r+c] = 1;
-			}
+
+				currentRun = currentRun->next; //get net run of current object
+			} while(currentRun != NULL); //end of current object
+		DrawString(ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY, 3, GIANT, RED, str);
 		}
 	}
 }
-
-
-void SetBackground() {
-	int r, c;
-
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-		}
-	}
-	//set all counters to zero
-	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
-}
-
 
 void Erode_3x3(int InIndex, int OutIndex)
 {
@@ -176,6 +220,9 @@ void DrawBoundingBoxes() {
 		if(ImgRegions.objects[o].area > MinArea) {
 			DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
 							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
+
 		}
+
 	}
+
 }
